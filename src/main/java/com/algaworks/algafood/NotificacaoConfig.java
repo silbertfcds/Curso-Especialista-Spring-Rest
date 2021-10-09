a480a224a5ff0a113eacc1c86b258a1cd3a10209
Silbert Firmino <silbert.fcds@hotmai.com>
2021-10-09 14:57:46 -0300

    Adicionando beans gerenciados pelo spring

--- src/main/java/com/algaworks/algafood/NotificacaoConfig.java
+++ src/main/java/com/algaworks/algafood/NotificacaoConfig.java
@@ -1,24 +1,19 @@
 package com.algaworks.algafood;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.algaworks.algafood.di.notificacao.NotificadorEmail;
-import com.algaworks.algafood.di.service.AtivacaoClienteService;
 
-//@Configuration
-public class AlgaConfig {
+@Configuration
+public class NotificacaoConfig {
 
 	@Bean
 	public NotificadorEmail notificadorEmail() {
 		NotificadorEmail notificador = new NotificadorEmail("smtp.algamail.com.br");
 		notificador.setCaixaAlta(true);
-
+		
 		return notificador;
 	}
-
-	@Bean
-	public AtivacaoClienteService ativacaoClienteService() {
-		return new AtivacaoClienteService(notificadorEmail());
-	}
-
+	
 }
