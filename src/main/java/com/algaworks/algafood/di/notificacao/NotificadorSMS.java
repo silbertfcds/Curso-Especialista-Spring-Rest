a66366b68c92f04fd1d8d5a7d170909abcc44fdb
Sousa <silbert.sousa@capgemini.com>
2023-01-27 09:00:41 -0300

    Desambiguação de beans com anotação customizada

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorSMS.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorSMS.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 
+@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
 public class NotificadorSMS implements Notificador {
 
