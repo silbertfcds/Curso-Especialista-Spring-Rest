5e083b570cffec7a4699432408253a69568d8b88
Sousa <silbert.sousa@capgemini.com>
2023-01-27 08:09:26 -0300

    Dependência opcional com @Autowired

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,19 +1,21 @@
 package com.algaworks.algafood.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
 
+@Component
 public class AtivacaoClienteService {
 
+	@Autowired(required = false)
 	private Notificador notificador;
-
-	public AtivacaoClienteService(Notificador notificador) {
-		this.notificador = notificador;
-	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
-
-		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+		if (notificador != null) {
+			notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+		}
 	}
 }
